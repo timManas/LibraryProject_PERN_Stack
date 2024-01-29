5742a37f5798b9351f821f3a29d843c974f7dc97
Tim M <timothy.romero.manas@gmail.com>
2024-01-28 21:36:30 -0500

    First commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const express = require('express');
+const app = express();
+const { Sequelize } = require('sequelize');
+const path = require('path');
+const cors = require('cors');
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../build')));
+
+// CONTROLLERS
+const booksController = require('./controllers/books_controller');
+app.use('/api/books', booksController);
+
+// LISTEN
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
